45541de1ca64fb4926704100e759e859ce62a724
josuerushanika <josuerushanika@gmail.com>
2023-09-14 18:32:38 +0200

    Building game app

--- game-app/src/components/GameCard.tsx
+++ game-app/src/components/GameCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 
 const GameCard = ({game}: Props) => {
   return (
-    <Card>
+    <Card borderRadius={10} overflow='hidden'>
         <Image  src={game.background_image}/>
         <CardBody>
-            <Heading>{game.name}</Heading>
+            <Heading fontSize='2xl'>{game.name}</Heading>
         </CardBody>
     </Card>
   )
--- game-app/src/components/GameGrid.tsx
+++ game-app/src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 
 const GameGrid = () => {
@@ -8,9 +9,9 @@ const {games, error} = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid column={3} spacing={10}>
+      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard  key={game.id} game={game} />
         ))}
       </SimpleGrid>
     </>
